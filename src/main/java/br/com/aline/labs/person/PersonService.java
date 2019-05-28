b3fcc4c81f298c56a5c1d555f021b62ac5131fb1
alinedias <aline.dias@ifood.com.br>
2019-05-28 00:30:08 -0300

    add person service

---
+++ src/main/java/br/com/aline/labs/person/PersonService.java
@@ -0,0 +1,52 @@
+package br.com.aline.labs.person;
+
+import br.com.aline.labs.facebook.FacebookClient;
+import br.com.aline.labs.facebook.UserFacebookResponse;
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+class PersonService {
+
+    private Logger LOG = LoggerFactory.getLogger(PersonService.class);
+    private PersonRepository personRepository;
+
+    private FacebookClient facebookClient;
+
+    @Value("${facebook.access.token}")
+    private String facebookAccessToken;
+
+    PersonService(PersonRepository personRepository, FacebookClient facebookClient) {
+        this.personRepository = personRepository;
+        this.facebookClient = facebookClient;
+    }
+
+    Person save(String facebookId) {
+        try {
+            UserFacebookResponse userFacebookResponse = facebookClient.getUser(facebookId, facebookAccessToken);
+            Person person = personRepository.save(new Person(userFacebookResponse.getId(), userFacebookResponse.getName()));
+            return person;
+        } catch (FeignException feignException) {
+            LOG.info("Feign error status: {}", feignException.status());
+            throw feignException;
+        }
+    }
+
+    List<Person> findAllWithLimit(Integer limit) {
+        return personRepository.findAll(PageRequest.of(0, limit)).getContent();
+    }
+
+    void delete(String facebookId) {
+        personRepository.deleteById(facebookId);
+    }
+
+    Person findById(String id) {
+        return personRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+}
